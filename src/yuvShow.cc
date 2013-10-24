7f64e4c5297164f651eecb669621b31299f3776d
Ilan Pegoraro <impegoraro@ua.pt>
2013-10-24 14:38:11 +0100

    yuvShow user friendly.
    	* yuvShow uses switch flags to be more user friendly.

--- src/yuvShow.cc
+++ src/yuvShow.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <getopt.h>
 #include <opencv2/opencv.hpp>
 
 #include "video.h"
@@ -13,10 +14,49 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-	bool cont = true;
+	bool cont = false, playing = true;
 	string ans;
-	if(argc > 1 && (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))) {
-		cerr<< "Usage: yuvShow <video>"<<endl<<endl;
+	bool showHelp = false;
+	char *src = NULL;
+	char nextOp;
+	const char* shortops = "rmhs:";
+	const struct option longops[] = {
+		"help", 0, NULL, 'h',
+		"manual-fps", 0, NULL, 'm',
+		"repeat", 0, NULL, 'r',
+		"source", 1, NULL, 's'
+	};
+
+	do {
+		nextOp = getopt_long(argc, argv, shortops, longops, NULL);
+		switch(nextOp) {
+			case 'h':
+				nextOp = -1;
+				showHelp = true;
+			break;
+			case 'r':
+				cont = true;
+			break;
+			case 'm':
+				playing = false;
+			break;
+			case 's':
+				src = optarg;
+			break;
+			case '?':
+			default:
+				showHelp = false;
+			break;
+		}
+	} while(nextOp !=- 1);
+
+	if(showHelp) {
+		cerr<< "Usage: yuvShow [OPTIONS] <video>"<<endl<<endl;
+		cout<<"[OPTIONS]"<<endl
+			<<"  -h, --help                    Shows this help message."<<endl
+			<<"  -s, --source                  Filename to read the video from. Missing this switch means to read from camera."<<endl
+			<<"  -r, --repeat                  Ask to repeat the video when finishes."<<endl
+			<<"  -m, --manual-fps              Use any key to step throughout the video instead of using the video's FPS."<<endl;
 		cout<< "The program is able to play videos in the following formats: RGB, YUV444, YUV422 and YUV420."<<endl;
 		cout<< "Univesidade de Aveiro 2013 - MIETC Audio and Video Coding"<<endl;
 		cout<< "Authors:"<<endl;
@@ -26,29 +66,27 @@ int main(int argc, char** argv)
 	}
 	try {
 		Video *v;
-		if(argc <= 1)
+		if(src == NULL)
 			v = new Video();
 		else {
-			string path(argv[1]);
+			string path(src);
 			v = new Video(path);
 		}
 		
-		while(cont) {
-			v->display();
+		v->display(playing);
 			
-			do {
-				cout<< "Do you want to play it again? [y/n]: ";
-				cin>> ans;
-				if(ans == "y" || ans == "Y") {
-					cont = true;
-					v->reset();
-					break;
-				}
-				else if(ans == "n" || ans == "N") {
-					cont = false;
-					break;
-				}
-			} while(true);
+		while(cont) {
+			cout<< "Do you want to play it again? [y/n]: ";
+			cin>> ans;
+			if(ans == "y" || ans == "Y") {
+				cont = true;
+				v->reset();
+			}
+			else if(ans == "n" || ans == "N") {
+				cont = false;
+				break;
+			}
+			v->display(playing);
 		}
 		delete v;
 	} catch (FileNotFoundException& e) {
